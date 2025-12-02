1d667ba50fd67965276eef0d553f80a2361b9fa6
felipemastex <felipemastex@gmail.com>
2025-12-02 16:38:37 -0300

    zero de novo

--- estudeaquiapp/android/app/src/main/kotlin/com/example/estudeaquiapp/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.estudeaquiapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- estudeaquiapp/lib/models/models.dart
+++
@@ -1,9 +0,0 @@
-// Main export file for all models
-export 'study_data.dart';
-export 'subject.dart';
-export 'topic.dart';
-export 'study_log_entry.dart';
-export 'pomodoro_settings.dart';
-export 'study_sequence.dart';
-export 'subject_template.dart';
-export 'schedule_plan.dart';
